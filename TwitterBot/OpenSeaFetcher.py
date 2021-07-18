3fe6ebdeac3515cf690ea54fe77972c1e5c467dc
ShivrajRana7 <rana.shiv68@gmail.com>
2021-07-18 23:07:09 +0530

    Tweeted with Media Img

--- TwitterBot/OpenSeaFetcher.py
+++ TwitterBot/OpenSeaFetcher.py
@@ -10,4 +10,24 @@ def OpenSeaFetchingSchema(token_id_of_nft, asset_contract_address_of_nft):
     response = requests.request("GET", url)
     # print(response.json())
     #print(json.dumps(response.json(), indent=4))
+    response2 = requests.request("GET", response.json()['token_metadata'])
+    # print(json.dumps(response2.json()['image_poster_url']))
+    getImage(response2.json()['image_poster_url'])
     return response.json()['permalink']
+
+
+def getImage(url):
+    with open('pic1.jpg', 'wb') as handle:
+        response = requests.get(url, stream=True)
+
+        if not response.ok:
+            print(response)
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+
+            handle.write(block)
+
+
+#OpenSeaFetchingSchema("3", "0x7c3a306e7e2adbc918ec8777d12335045471b110")
