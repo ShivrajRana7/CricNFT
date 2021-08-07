f8cf016fe4d4925914463767bd2a92f6abdc9cfb
ShivrajRana7 <rana.shiv68@gmail.com>
2021-08-07 15:24:20 +0530

    Build V1

--- TwitterBot/keys.py
+++
@@ -1,8 +0,0 @@
-# keys_format.py shares the same format as keys.py.
-# This file is not meant to be used in the main script.
-# Instead, copy this file into keys.py and put your own
-# keys there.
-CONSUMER_KEY = "5Epf86Accj4eGvSIgPw48mkqg"
-CONSUMER_SECRET = "j0yOdPBDrP5OJxrF6ULBSaXhKxSHig0xAVu72BA8HuBruWaZZt"
-ACCESS_KEY = "1415630170409488386-0IjV6QZN1dlF1DJ8RKgd8JAooXeuN9"
-ACCESS_SECRET = "CGZxnAl37RFG9gJhGoEOo2Mmdwk3yGCUvubLbvNQviVzO"
